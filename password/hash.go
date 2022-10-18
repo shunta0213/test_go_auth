78ca6af4a06a8f16998ab0462eeed31896d6954a
shunta0213 <shuntaide16213@gmail.com>
2022-10-19 00:01:58 +0900

    added hash function

---
+++ password/hash.go
@@ -0,0 +1,34 @@
+package password
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+/*
+Hash function using bcrypt
+*/
+func HashPassword(pw string) string {
+	pwByte := []byte(pw)
+	hashed, err := bcrypt.GenerateFromPassword(pwByte, 10)
+
+	if err != nil {
+
+	}
+	return string(hashed)
+}
+
+/*
+Compare  a raw password and hash got from Database.
+
+If they are matched, it return nil.
+Else, it return bcrypt.ErrMismatchedHashAndPassword
+*/
+
+func ComparePasswordAndHash(pw string, hash string) error {
+	pwByte := []byte(pw)
+	hashByte := []byte(pw)
+
+	err := bcrypt.CompareHashAndPassword(hashByte, pwByte)
+
+	return err
+}
