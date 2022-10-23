ad4769f1808f19e62055d4da6b2ef6c22b362c5c
shunta0213 <shuntaide16213@gmail.com>
2022-10-23 23:27:17 +0900

    Added route

--- server/ginserver/route.go
+++ server/ginserver/route.go
@@ -1,12 +1,15 @@
 package ginserver
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shunta0213/test_go_auth/server/ginserver/controllers"
+	"github.com/shunta0213/test_go_auth/server/ginserver/services"
 )
 
-func SetRoutes(r *gin.Engine) {
+func SetRoutes(r *gin.Engine, DB *sql.DB) {
 
 	// For Check Api connections
 	r.GET("/", func(ctx *gin.Context) {
@@ -15,9 +18,7 @@ func SetRoutes(r *gin.Engine) {
 		})
 	})
 
-	// SignIn
-	r.GET("/signin", func(ctx *gin.Context) {})
-
-	// SingUp
-	r.POST("/signup", func(ctx *gin.Context) {})
+	userController := controllers.NewUserController(services.NewUserService(DB))
+	r.GET("/signin", func(ctx *gin.Context) { userController.SignIn(ctx) })
+	r.POST("/signup", func(ctx *gin.Context) { userController.SignUp(ctx) })
 }
