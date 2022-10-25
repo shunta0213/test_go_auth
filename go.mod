e79391d6408e7ce11a0e2d5dfa1e5ff11dfac598
shunta0213 <shuntaide16213@gmail.com>
2022-10-25 12:05:31 +0900

    added godotenv to load env file in local

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/friendsofgo/errors v0.9.2
 	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0
 	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
 	github.com/lib/pq v1.10.7
 	github.com/spf13/viper v1.13.0
--- server/connect_database.go
+++ server/connect_database.go
@@ -5,12 +5,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	cLog "github.com/shunta0213/test_go_auth/log"
 )
 
 func ConnectDatabase() (*sql.DB, error) {
 	uri := loadConfig()
+	if uri == "" {
+		return nil, fmt.Errorf("Could not read .env file")
+	}
 	cLog.Println(uri)
 	db, err := sql.Open("postgres", uri)
 	if err != nil {
@@ -20,6 +24,13 @@ func ConnectDatabase() (*sql.DB, error) {
 }
 
 func loadConfig() string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		cLog.Fatalf("Could not load .env file:", err)
+		return ""
+	}
+
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s database=%s",
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 }
